535af20c23a9576dd4801af782ea88bfbe017f57
trustymalware <mehdi.benseghir@trustymalware.info>
2018-07-13 16:23:34 +0200

    js

--- dist/script/built.js
+++ dist/script/built.js
@@ -1,17 +1,17 @@
-function myFunction() {
-    document.getElementsByClassName("dropbutton").classList.toggle("show");
-};
+// function myFunction() {
+//     document.getElementByID("maincv").classList.toggle("show");
+// };
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbutton')) {
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbutton')) {
   
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  };
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   };
